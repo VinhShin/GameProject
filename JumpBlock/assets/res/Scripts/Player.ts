1ee7ea2d5d2d5353e2d2c4e6c51b4e44ff686901
Nguyen Thanh Quang <quang2172@gmail.com>
2018-04-25 13:46:00 +0700

    no message

--- JumpBlock/assets/res/Scripts/Player.ts
+++ JumpBlock/assets/res/Scripts/Player.ts
@@ -5,21 +5,14 @@ export default class Player extends cc.Component {
 
     @property speed: number = 0;
 
-    camera: cc.Camera;
-    anim: cc.Animation;
-
     onLoad() {
 
     }
 
     start() {
-        this.anim = this.getComponent(cc.Animation);
     }
 
     jumpAction(jump, Distance_X, Distance_Y) {
-        this.anim.play("PlayerJump");
-        this.node.runAction(cc.jumpBy(2, Distance_X, Distance_Y, jump, 1).easing(cc.easeCubicActionInOut()));
-        cc.delayTime(2);
-        this.anim.play("PlayerIdle");
+        this.node.runAction(cc.jumpBy(1, Distance_X, Distance_Y, jump, 1).easing(cc.easeCubicActionInOut()));
     }
 }
