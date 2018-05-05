3a585130595c4bbd2d3668ebcfb69b79f64c1cd5
TranLeVanDuc <vanduc3997@gmail.com>
2018-05-05 12:02:21 +0700

    aaaa

--- Snake/assets/Scripts/GameManager.ts
+++ Snake/assets/Scripts/GameManager.ts
@@ -7,10 +7,22 @@ export default class NewClass extends cc.Component {
 
     Snake: cc.Node[] = [];
     timer: number;
-
+    Food: cc.Node[] = [];
     color: cc.Color[] = [cc.Color.RED, cc.Color.BLUE, cc.Color.WHITE, cc.Color.GREEN, cc.Color.ORANGE,
     cc.Color.YELLOW, cc.Color.MAGENTA];
-
+    timeFood: number;
+    lifeFood: boolean;
+    createFood()
+    {
+        while(this.Food.length < 5)  {
+            var food = cc.instantiate(this.prefabSnake);
+            food.setPosition(cc.rand() % 480, cc.rand() % 480);
+            food.color = this.color[Math.floor(cc.rand() % this.color.length)];
+            this.Food.push(food);
+        }
+       
+    }
+    
     onLoad() {
         // init logic
         for (let i = 0; i < 5; i++) {
@@ -20,24 +32,42 @@ export default class NewClass extends cc.Component {
             snake.color = this.color[Math.floor(cc.rand() % this.color.length)];
             this.Snake.push(snake);
         }
-        
+        this.createFood();
     }
-
+    spawnFood(){
+            this.Food[0].destroy();
+            this.Food.reverse();
+            this.Food.pop();
+            this.Food.reverse();
+            this.createFood();
+            this.timeFood = 0;
+            this.lifeFood = false;
+    }
     start() {
         this.timer = 0;
+        this.timeFood = 0;
+        this.lifeFood = true;
+        this.node.addChild(this.Food[0]);
     }
-
     update(dt) {
         this.timer += dt;
         if (this.timer >= 0.5) {
             for (let s of this.Snake) {
                 s.setPositionX(s.x + 32);
             }
-
-            for (let s = 0;)
-
             this.timer = 0;
         }
+        this.timeFood += dt;
+        if(!this.lifeFood)
+        {   
+            this.node.addChild(this.Food[0]);
+            this.lifeFood = true;
+        }
+        else if(this.timeFood > 2)
+        {
+            this.spawnFood();
+        }
+       
         
     }
 }
